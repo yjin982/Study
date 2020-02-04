b665a2f01b11b18512a6db1ab914d5596b615658
yjin982 <yjin.jeong982@gmail.com>
2020-02-04 17:23:48 +0900

    200204

---
+++ javapro2/연습문제/DTO Practice/DtoProduct.java
@@ -0,0 +1,26 @@
+package pack8;
+
+public class DtoProduct {
+	private String productName;
+	private int area, num;
+	
+	
+	public int getArea() {
+		return area;
+	}
+	public void setArea(int area) {
+		this.area = area;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
