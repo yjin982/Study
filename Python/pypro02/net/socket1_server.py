a7f370479e8ca613c392e72bf937ff9daa6f498a
Yujin Jeong <57245987+yjin982@users.noreply.github.com>
2020-05-13 17:53:50 +0900

    Update socket1_server.py

--- Python/pypro02/net/socket1_server.py
+++ Python/pypro02/net/socket1_server.py
@@ -3,7 +3,7 @@ from socket import *
 
 #server 구축
 serversock = socket(AF_INET, SOCK_STREAM) #소켓의 종류와 유형
-serversock.bind(('192.168.0.61', 9999))  #bind 할때는 튜플로
+serversock.bind(('127.0.0.1', 9999))  #bind 할때는 튜플로
 serversock.listen(1) #1 ~ 5 : 동시 접속수
 
 print('server start...')
