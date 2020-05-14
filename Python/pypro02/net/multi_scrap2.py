7a3ee946b8fea268792aa41a72644055b17ab797
Yujin Jeong <57245987+yjin982@users.noreply.github.com>
2020-05-14 14:09:25 +0900

    Add files via upload

--- Python/pypro02/net/multi_scrap2.py
+++ Python/pypro02/net/multi_scrap2.py
@@ -1,15 +1,16 @@
 '''
-멀티 프로세싱을 통한 웹 스크랩핑 : multiprocessing X
+멀티 프로세싱을 통한 웹 스크랩핑 : multiprocessing O
 (스크랩핑:1번만 긁어옴, 크롤링:주기적으로 긁어옴)
 '''
 import requests, time
 from bs4 import BeautifulSoup as bs
+from multiprocessing import Pool
 
 def get_link():
     url = "https://beomi.github.io/beomi.github.io_old/"
     data = requests.get(url).text
     soup = bs(data, 'html.parser')
-    #print(soup)
+    
     my_title = soup.select('h3 > a')
     data = []
     
@@ -28,9 +29,9 @@ def get_content(link):
 if __name__ == '__main__':
     start_time = time.time()
     
-    for link in get_link():
-        get_content(link)
+    pool = Pool(processes = 4)
+    pool.map(get_content, get_link())
     
     print('처리 시간 : %s초'%(time.time() - start_time))
-
-#처리 시간 : 24.516031742095947초
+    
+#처리 시간 : 6.36165452003479초
